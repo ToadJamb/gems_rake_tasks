fd78063ea959287e8487ffc8ca0692dd04cdaf69
Travis Herrick <tthetoad@gmail.com>
2011-08-07 22:13:13 -0400

    Modified the rdoc task to only generate rdoc tasks if files match the patterns and set the default task to rdoc:app.

--- lib/rake_tasks/rdoc.rb
+++ lib/rake_tasks/rdoc.rb
@@ -17,21 +17,25 @@ namespace :rdoc do
 
   # Loop through the typs of rdoc files to generate an rdoc task for each one.
   rdoc_files.keys.each do |rdoc_task|
-    Rake::RDocTask.new(
-        :rdoc         => rdoc_task,
-        :clobber_rdoc => "#{rdoc_task}:clobber",
-        :rerdoc       => "#{rdoc_task}:force") do |rdtask|
-      rdtask.rdoc_dir = File.join(base_path, rdoc_task.to_s)
-      rdtask.options << '--charset' << 'utf8'
-      rdtask.rdoc_files.include(rdoc_files[rdoc_task], 'README')
-      rdtask.main = 'README'
-    end
+    unless Dir[*rdoc_files[rdoc_task]].length == 0
+      Rake::RDocTask.new(
+          :rdoc         => rdoc_task,
+          :clobber_rdoc => "#{rdoc_task}:clobber",
+          :rerdoc       => "#{rdoc_task}:force") do |rdtask|
+        rdtask.rdoc_dir = File.join(base_path, rdoc_task.to_s)
+        rdtask.options << '--charset' << 'utf8'
+        rdtask.rdoc_files.include(rdoc_files[rdoc_task], 'README')
+        rdtask.main = 'README'
+      end
 
-    Rake::Task[rdoc_task].comment =
-      "Generate #{rdoc_task} RDoc documentation."
+      Rake::Task[rdoc_task].comment =
+        "Generate #{rdoc_task} RDoc documentation."
+    end
   end
 
   CLOBBER.include(base_path)
 ################################################################################
 end # :rdoc
 ################################################################################
+
+task :default => ["rdoc:app"]
