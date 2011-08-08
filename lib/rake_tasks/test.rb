a7bb75f2d1d4791677447610d30cc5728568ffe6
Travis Herrick <tthetoad@gmail.com>
2011-08-07 22:15:17 -0400

    Modified the test tasks to use an if/end statement instead of a break and set the default task to test:all if tests are found.

--- lib/rake_tasks/test.rb
+++ lib/rake_tasks/test.rb
@@ -1,40 +1,44 @@
-################################################################################
-namespace :test do
-################################################################################
-  test_dir = 'test'
-  break unless File.directory?(test_dir)
+test_dir = 'test'
 
-  # Add a task to run all tests.
-  Rake::TestTask.new('all') do |task|
-    task.pattern = "#{test_dir}/*_test.rb"
-    task.verbose = true
-    task.warning = true
-  end
-  Rake::Task[:all].comment = 'Run all tests'
-
-  file_list = Dir["#{test_dir}/*_test.rb"]
-
-  # Add a distinct test task for each test file.
-  file_list.each do |item|
-    # Get the name to use for the task by removing '_test.rb' from the name.
-    task_name = File.basename(item, '.rb').gsub(/_test$/, '')
-
-    # Add each test.
-    Rake::TestTask.new(task_name) do |task|
-      task.pattern = item
+if File.directory?(test_dir)
+  ############################################################################
+  namespace :test do
+  ############################################################################
+    # Add a task to run all tests.
+    Rake::TestTask.new('all') do |task|
+      task.pattern = "#{test_dir}/*_test.rb"
       task.verbose = true
       task.warning = true
     end
-  end
+    Rake::Task[:all].comment = 'Run all tests'
 
-  file_name = File.basename(Dir.getwd) + '_test.rb'
+    file_list = Dir["#{test_dir}/*_test.rb"]
 
-  if File.file?("#{test_dir}/#{file_name}")
-    desc "Run a single method in #{file_name}."
-    task :method, [:method_name] do |t, args|
-      puts `ruby ./#{test_dir}/#{file_name} --name #{args[:method_name]}`
+    # Add a distinct test task for each test file.
+    file_list.each do |item|
+      # Get the name to use for the task by removing '_test.rb' from the name.
+      task_name = File.basename(item, '.rb').gsub(/_test$/, '')
+
+      # Add each test.
+      Rake::TestTask.new(task_name) do |task|
+        task.pattern = item
+        task.verbose = true
+        task.warning = true
+      end
     end
-  end
-################################################################################
-end # :test
-################################################################################
+
+    file_name = File.basename(Dir.getwd) + '_test.rb'
+
+    if File.file?("#{test_dir}/#{file_name}")
+      desc "Run a single method in #{file_name}."
+      task :method, [:method_name] do |t, args|
+        puts `ruby ./#{test_dir}/#{file_name} --name #{args[:method_name]}`
+      end
+    end
+  ############################################################################
+  end # :test
+  ############################################################################
+
+  Rake::Task[:default].prerequisites.clear
+  task :default => 'test:all'
+end
