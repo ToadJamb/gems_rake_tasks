e55ea59966c54045b8dfee9f637c8f028abb2f16
Travis Herrick <tthetoad@gmail.com>
2012-06-10 01:55:29 -0700

    Convert README to README.md.

--- test/integration/doc_integration_test.rb
+++ test/integration/doc_integration_test.rb
@@ -57,7 +57,7 @@ class DocIntegrationTest < Test::Unit::TestCase
   end
 
   def test_actual_readme_contents
-    contents = File.open('README', 'r')
+    contents = File.open('README.md', 'r')
     assert_contains contents.read,
       "gem '#{@gem_spec.name}', '~> #{@gem_spec.version}'"
   end
