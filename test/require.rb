6b509e4b0def6aa88dc391d64d3c2ff610b875c1
Travis Herrick <tthetoad@gmail.com>
2012-06-16 20:53:32 -0700

    Gemspec name is based on gemspec instead of root folder.
    --HG--
    branch : feature/30961337-test-failures-when-root-folder-changes
    rename : test/unit/gem_unit_test.rb => test/unit/gem_test.rb

--- test/require.rb
+++ test/require.rb
@@ -2,6 +2,8 @@ require 'test/unit'
 
 require 'mocha'
 require 'fakefs/safe'
+require 'test_unit_helper'
 
-require_relative File.join('../lib/rake_tasks'.split('/'))
-require_relative File.join('./lib/rake_tasks_shared'.split('/'))
+require_relative './lib/rake_tasks_shared'
+
+require_relative '../lib/rake_tasks'
