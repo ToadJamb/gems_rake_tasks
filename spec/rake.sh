66d5f8c51cfd9dd1f5c0108251a0cbe6594311d1
Travis Herrick <tthetoad@gmail.com>
2015-01-02 02:41:29 -0800

    Add a script for running against other rubies

---
+++ spec/rake.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  source "$HOME/.rvm/scripts/rvm"
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+  source "/usr/local/rvm/scripts/rvm"
+else
+  printf "ERROR: An RVM installation was not found.\n"
+fi
+
+rubies=(
+  1.9.3-p547
+  2.0.0-p481
+  2.1.2
+)
+
+header='****************************************'
+footer='----------------------------------------'
+
+for i in "${rubies[@]}"
+do
+  echo "$header$header"
+  rvm use $i
+  rvm current
+  bundle check || bundle install --path vendor
+  bundle exec rake
+  echo "$footer$footer"
+  echo
+done
