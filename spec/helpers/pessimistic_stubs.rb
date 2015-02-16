ead31deb80070ab6781c0678f5b75f40863cad14
Travis Herrick <tthetoad@gmail.com>
2015-02-15 17:28:46 -0800

    Roll pessimistic stubbing (from rspec_hooks) into one new helper

---
+++ spec/helpers/pessimistic_stubs.rb
@@ -0,0 +1,17 @@
+require File.expand_path('spec/helpers/mocha_helpers')
+
+RSpec.configure do |config|
+  config.before do
+    unsafe_methods = RakeTasks::System.methods - Module.methods
+    unsafe_methods.each do |method|
+      not_implemented RakeTasks::System, method
+    end
+  end
+
+  config.before do
+    util_methods = Util.methods - Util.superclass.methods
+    util_methods.each do |method|
+      not_implemented Util, method
+    end
+  end
+end
