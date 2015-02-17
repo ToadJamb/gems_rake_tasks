d4ef1edfc0abb2c9a2b50f1eaa086b5002c97008
Travis Herrick <tthetoad@gmail.com>
2015-02-16 19:11:07 -0800

    Double unsafe classes/modules instead of mocking each method

--- spec/helpers/pessimistic_stubs.rb
+++ spec/helpers/pessimistic_stubs.rb
@@ -2,16 +2,11 @@ require File.expand_path('spec/helpers/boundary_helpers')
 
 RSpec.configure do |config|
   config.before do
-    unsafe_methods = RakeTasks::System.methods - Module.methods
-    unsafe_methods.each do |method|
-      not_implemented RakeTasks::System, method
-    end
-  end
-
-  config.before do
-    util_methods = Util.methods - Util.superclass.methods
-    util_methods.each do |method|
-      not_implemented Util, method
+    [
+      RakeTasks::System,
+      Util,
+    ].each do |klass|
+      stub_const klass.to_s, class_double(klass).as_stubbed_const
     end
   end
 end
