0193714274ec76f5976b4d03f38ef6b023f1455d
Travis Herrick <tthetoad@gmail.com>
2016-07-31 23:14:28 -0700

    run the default rake task as a prerequisite for the release task

--- lib/rake_tasks/tasks/release.rb
+++ lib/rake_tasks/tasks/release.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 if RakeTasks::Gem.gemspec_file?
   desc 'Prepare a gem for release'
-  task :release do |task|
+  task :release => :default do |task|
     RakeTasks::Release.release
   end
 end
