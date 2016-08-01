8bea282a4e20002f7a2f8a1fd2e3b7f546d1beed
Travis Herrick <tthetoad@gmail.com>
2016-07-31 23:58:27 -0700

    remove the checksum task

--- lib/rake_tasks/tasks/checksum.rb
+++ lib/rake_tasks/tasks/checksum.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 if RakeTasks::Gem.gem_file?
-  require 'digest/sha2'
-
-  desc 'Generate a checksum for the current gem file'
-  task :checksum do |task|
-    message = "The checksum rake task will be deprecated in version 5.\n"
-    message += "Please use `rake checksums`"
-    warn message
-
-    RakeTasks::Checksum.save_checksum_for :sha512
-  end
-
   desc 'Generate all checksums for the current gem file'
   task :checksums do |task|
     RakeTasks::Checksum.checksums
